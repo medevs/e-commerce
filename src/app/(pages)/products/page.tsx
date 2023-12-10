06879b0bef192d730adaac5ffc6d238790c1a198
ahmed <oublihi.a@gmail.com>
2023-12-10 10:52:28 +0100

    feat: add filters to product page

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -7,6 +7,7 @@ import { fetchDocs } from '../../_api/fetchDocs'
 import { Blocks } from '../../_components/Blocks'
 import { Gutter } from '../../_components/Gutter'
 import { HR } from '../../_components/HR'
+import Filters from './Filters'
 
 import classes from './index.module.scss'
 
@@ -31,6 +32,7 @@ const Products = async () => {
   return (
     <div className={classes.container}>
       <Gutter className={classes.products}>
+        <Filters categories={categories} />
         <Blocks blocks={page?.layout} disableTopPadding={true} />
       </Gutter>
       <HR />
